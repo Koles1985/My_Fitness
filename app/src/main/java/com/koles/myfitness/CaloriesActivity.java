a58bbbf46211a0348b9797d828cafefa7540504c
Koles1985 <Koles1985>
2022-07-17 18:05:36 +0300

    add alertDialog

--- app/src/main/java/com/koles/myfitness/CaloriesActivity.java
+++ app/src/main/java/com/koles/myfitness/CaloriesActivity.java
@@ -2,11 +2,17 @@ package com.koles.myfitness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,11 +27,15 @@ public class CaloriesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calories);
+        setTitle("Калорийность продуктов");
         foodList = (ListView) findViewById(R.id.listView);
         addCategoryButton = (Button) findViewById(R.id.button_addCategory);
         addCategoryButton.setOnClickListener(v -> {
-            Intent intent = new Intent(this, AddFoodCategoryActivity.class);
-            startActivity(intent);
+            //Intent intent = new Intent(this, AddFoodCategoryActivity.class);
+            //startActivity(intent);
+            AddCategoryDialog dialog = new AddCategoryDialog();
+            dialog.show(getSupportFragmentManager(), "My Dialog");
+            System.out.println(dialog.getMyDialog() == null);
         });
         list = new ArrayList<>(Arrays.asList(getFoodCategoryArray()));
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
